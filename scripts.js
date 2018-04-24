fa02192ebad19b8264b538f771c840fee696b708
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-04-24 22:00:12 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -2,12 +2,9 @@ $(document).ready(function(){
 	console.log('DOM loaded - you can have fun');
 
     var span = $('span');
-        span.each(function(index, element) {
-    });
-
     span.each(function(index, element) {
         if(index % 2 == 0) {
-            $(element).css('color', 'red');
+        $(element).css('color', 'red');
         };
     });
 
